86dca0357d88c8b1f03af1cafb5cd3f657b5dfb6
QUENTIN ROYER <royerquentin.pro@outlook.com>
2021-03-17 22:30:30 +0100

    Encore un peu plus "S"

--- Program.cs
+++ Program.cs
@@ -5,31 +5,35 @@ namespace FizzBuzz
     class Program
     {
         // Main = Orchestrateur
+        // Si je devais changer l'affichage console en affichage Web, qu'est-ce qui change ?
+        // - Console.WriteLine qui disparait
+        // - Methode StringBuilder change si on veux changer le format
         static void Main(string[] args)
         {
             // Nombre d'itérations: Sortir le for
-            // Affichage:
 
             int depart = 1;
             int fin = 100;
             int pas = 1;
+            IExporter exporter = new ConsoleExporter();
+
             for (int i = depart; i <= fin; i+=pas)
             {
                 if (Condition.FizzBuzz(i))
                 {
-                    Console.WriteLine($"{i} : FizzBuzz");
+                    exporter.Export(StringBuilder.FizzBuzz(i));
                     continue;
                 }
 
                 else if (Condition.Fizz(i))
                 {
-                    Console.WriteLine($"{i} : Fizz");
+                    exporter.Export(StringBuilder.Fizz(i));
                     continue;
                 }
 
                 else if (Condition.Buzz(i))
                 {
-                    Console.WriteLine($"{i} : Buzz");
+                    exporter.Export(StringBuilder.Buzz(i));
                     continue;
                 }
             }
@@ -38,6 +42,32 @@ namespace FizzBuzz
         
     }
 
+    public interface IExporter
+    {
+        void Export(string builded);
+    }
+    public class ConsoleExporter : IExporter {
+        public void Export(string builded) {
+            Console.WriteLine(builded);
+        }
+    }
+
+    public class WebExporter : IExporter {
+        public void Export(string builded) {}
+    }
+    // Les deux classes sont très semblable, devrait-on faire qqchose ?
+    public class StringBuilder {
+        public static string Fizz(int input) {
+            return $"{input} Fizz";
+        }
+        public static string Buzz(int input) {
+            return $"{input} Buzz";
+        }
+
+        public static string FizzBuzz(int input) {
+            return $"{input} FizzBuzz";
+        }
+    }
     public class Condition {
         public static bool Fizz(int input) { return input % 3 == 0; }
         public static bool Buzz(int input) { return input % 5 == 0; }
